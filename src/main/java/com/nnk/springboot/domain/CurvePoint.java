287d9aa52af755bc2b22d6b48c9ba27388f19f60
David <davidroussel171113@outlook.fr>
2025-05-14 00:00:23 +0200

    Commit initial base de code

---
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -0,0 +1,17 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+
+
+@Entity
+@Table(name = "curvepoint")
+public class CurvePoint {
+    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+}
