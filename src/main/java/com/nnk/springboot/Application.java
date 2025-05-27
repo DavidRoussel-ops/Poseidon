638f3420476f0455233cbe54a44ddde2c19a2011
David <davidroussel171113@outlook.fr>
2025-05-27 23:03:26 +0200

    Ajout d'un SysOut pour prévenir du lancement de l'application

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot;
 
+import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("L'application est lancer");
 	}
 
 }
