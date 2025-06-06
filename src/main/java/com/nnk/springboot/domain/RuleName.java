8f116108a62bc6f18249f3a7f09f72f675845824
David <davidroussel171113@outlook.fr>
2025-06-06 10:06:00 +0200

    Ajout d'un constructeur par défaut

--- src/main/java/com/nnk/springboot/domain/RuleName.java
+++ src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -29,6 +29,10 @@ public class RuleName {
     @Column(name = "sqlPart")
     private String sqlPart;
 
+    public RuleName() {
+        super();
+    }
+
     public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
         this.name = name;
         this.description = description;
