6a0619fc1ed6f4c3889654c7f80ee44aba8cdede
David <davidroussel171113@outlook.fr>
2025-07-01 23:11:34 +0200

    Remise au propre de la page d'application

--- src/main/java/com/nnk/springboot/Application.java
+++ src/main/java/com/nnk/springboot/Application.java
@@ -1,6 +1,5 @@
 package com.nnk.springboot;
 
-import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
