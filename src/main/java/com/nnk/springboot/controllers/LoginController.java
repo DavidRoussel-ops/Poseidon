59a78d6b236c50be1a0d52e0606e086eda38c3d6
David <davidroussel171113@outlook.fr>
2025-07-08 23:09:55 +0200

    Modification controller pour le login afin d'utiliser les méthodes mises en place dans le SecurityService class

--- src/main/java/com/nnk/springboot/controllers/LoginController.java
+++ src/main/java/com/nnk/springboot/controllers/LoginController.java
@@ -1,7 +1,14 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
+import com.nnk.springboot.service.SecurityService;
+import com.nnk.springboot.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +18,34 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("app")
 public class LoginController {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
     @GetMapping("login")
-    public ModelAndView login() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("login");
-        return mav;
+    public ModelAndView login(HttpSession httpSession) {
+        ModelAndView mavLogin = new ModelAndView();
+        mavLogin.setViewName("login");
+        ModelAndView mavBidlist = new ModelAndView();
+        mavBidlist.setViewName("bidList/list");
+        if (!securityService.isAuthenticated()) {
+            return mavLogin;
+        }
+        UserDetails userDetails = securityService.getCurrentUserDetails();
+        if (userDetails == null) {
+            return mavLogin;
+        }
+        User user = userService.getUserByUsername(userDetails.getUsername());
+        httpSession.setAttribute("user", user);
+        logger.info("Utilisateur connecter : {}", user);
+        return mavBidlist;
     }
 
     @GetMapping("secure/article-details")
