6ba65adc2b19fa4a1e77dc957ab8c0267338825a
David <davidroussel171113@outlook.fr>
2025-07-21 23:27:57 +0200

    Clean code

--- src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -28,7 +28,7 @@ public class CurveController {
     }
 
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addCurvePointForm(CurvePoint curvePoint) {
         return "curvePoint/add";
     }
 
