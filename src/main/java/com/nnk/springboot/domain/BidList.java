f3d655a1c1ec69cbc1c01e8a1051ad36da594534
David <davidroussel171113@outlook.fr>
2025-08-02 21:20:58 +0200

    Correction orthographe

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -23,7 +23,7 @@ public class BidList {
     @Column(name = "type")
     private String type;
 
-    @NotNull(message = "Le champ bidQQuantity doit être renseigner")
+    @NotNull(message = "Le champ bidQuantity doit être renseigner")
     @Column(name = "bidQuantity")
     private Double bidQuantity;
 
