93b4ad615231d331d4ede2a45debf73a8b2ad1f4
David <davidroussel171113@outlook.fr>
2025-08-04 22:34:18 +0200

    Modification nom des méthodes d'update et de delete

--- src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -53,8 +53,8 @@ public class CurveController {
     }
 
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
-                             BindingResult result, Model model) {
+    public String updateCurvePoint(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
+                                   BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
         if (result.hasErrors()) {
             return "curvePoint/update";
@@ -66,7 +66,7 @@ public class CurveController {
     }
 
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteCurvePoint(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
         CurvePoint curvePoint = curvePointService.getCurvePointById(id).orElseThrow(() -> new IllegalArgumentException("Invalid curvePoint id : " + id));
         curvePointService.deleteCurvePointById(curvePoint.getId());
