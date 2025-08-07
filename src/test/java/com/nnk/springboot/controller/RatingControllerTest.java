7a026c6dd033398c8fca7e46e80e7f36b84ee10b
David <davidroussel171113@outlook.fr>
2025-08-07 18:14:07 +0200

    Ajout de cas de test

--- src/test/java/com/nnk/springboot/controller/RatingControllerTest.java
+++ src/test/java/com/nnk/springboot/controller/RatingControllerTest.java
@@ -19,12 +19,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -98,5 +100,116 @@ public class RatingControllerTest {
         mockMvc.perform(get("/rating/add"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("rating"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testValidate() throws Exception {
+        User user = new User();
+        user.setFullname("test");
+        user.setUsername("test");
+        user.setPassword(encoder.encode("test"));
+        user.setRole("ADMIN");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("ADMIN", user);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(user.getUsername());
+        when(securityService.isAuthenticated()).thenReturn(true);
+        when(securityService.getCurrentUserDetails()).thenReturn(userDetails);
+        when(userService.getUserByUsername(userDetails.getUsername())).thenReturn(user);
+        mockMvc.perform(post("/rating/validate")
+                        .param("moodysRating", String.valueOf(1))
+                        .param("sandPRating", String.valueOf(2))
+                        .param("fitchRating", String.valueOf(3))
+                        .param("orderNumber", String.valueOf(4)))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/rating/list"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testValidateNull() throws Exception {
+        User user = new User();
+        user.setFullname("test");
+        user.setUsername("test");
+        user.setPassword(encoder.encode("test"));
+        user.setRole("ADMIN");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("ADMIN", user);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(user.getUsername());
+        when(securityService.isAuthenticated()).thenReturn(true);
+        when(securityService.getCurrentUserDetails()).thenReturn(userDetails);
+        when(userService.getUserByUsername(userDetails.getUsername())).thenReturn(user);
+        mockMvc.perform(post("/rating/validate"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/rating/add"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testShowUpdateForm() throws Exception {
+        User user = new User();
+        user.setFullname("test");
+        user.setUsername("test");
+        user.setPassword(encoder.encode("test"));
+        user.setRole("ADMIN");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("ADMIN", user);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(user.getUsername());
+        when(securityService.isAuthenticated()).thenReturn(true);
+        when(securityService.getCurrentUserDetails()).thenReturn(userDetails);
+        when(userService.getUserByUsername(userDetails.getUsername())).thenReturn(user);
+        when(ratingService.getRatingById(anyInt())).thenReturn(Optional.ofNullable(mock(Rating.class)));
+        mockMvc.perform(get("/rating/update/" + 1))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("rating"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateRating() throws Exception {
+        User user = new User();
+        user.setFullname("test");
+        user.setUsername("test");
+        user.setPassword(encoder.encode("test"));
+        user.setRole("ADMIN");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("ADMIN", user);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(user.getUsername());
+        when(securityService.isAuthenticated()).thenReturn(true);
+        when(securityService.getCurrentUserDetails()).thenReturn(userDetails);
+        when(userService.getUserByUsername(userDetails.getUsername())).thenReturn(user);
+        when(ratingService.getRatingById(anyInt())).thenReturn(Optional.ofNullable(mock(Rating.class)));
+        mockMvc.perform(post("/rating/update/" + 1)
+                        .param("moodysRating", String.valueOf(1))
+                        .param("sandPRating", String.valueOf(2))
+                        .param("fitchRating", String.valueOf(3))
+                        .param("orderNumber", String.valueOf(5)))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/rating/list"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testDeleteRating() throws Exception {
+        User user = new User();
+        user.setFullname("test");
+        user.setUsername("test");
+        user.setPassword(encoder.encode("test"));
+        user.setRole("ADMIN");
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("ADMIN", user);
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn(user.getUsername());
+        when(securityService.isAuthenticated()).thenReturn(true);
+        when(securityService.getCurrentUserDetails()).thenReturn(userDetails);
+        when(userService.getUserByUsername(userDetails.getUsername())).thenReturn(user);
+        when(ratingService.getRatingById(anyInt())).thenReturn(Optional.ofNullable(mock(Rating.class)));
+        mockMvc.perform(get("/rating/delete/" + 1))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/rating/list"));
     }
 }
