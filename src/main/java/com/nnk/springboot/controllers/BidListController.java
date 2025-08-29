0d019defa7b837061cac357e10b84faf56bc2c25
David <davidroussel171113@outlook.fr>
2025-08-29 20:02:49 +0200

    Clean Code

--- src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -10,32 +10,44 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
-import java.sql.Timestamp;
-
-
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
+
     @Autowired
     private BidListService bidListService;
 
+    /**
+     * Méthode get de la page home de l'objet BidList
+     * @param model
+     * @return String
+     */
     @RequestMapping("/bidList/list")
     public String home(Model model)
     {
-        // TODO: call service find all bids to show to the view
         model.addAttribute("bidLists", bidListService.getBidLists());
         return "bidList/list";
     }
 
+    /**
+     * Méthode get de la page add de l'objet BidList
+     * @param model
+     * @return String
+     */
     @GetMapping("/bidList/add")
     public String addBidForm(Model model) {
         model.addAttribute("bidList", new BidList());
         return "bidList/add";
     }
 
+    /**
+     * Méthode post de l'objet BidList
+     * @param bid
+     * @param result
+     * @param model
+     * @return String
+     */
     @PostMapping("/bidList/validate")
     public String validate(@Valid BidList bid, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return bid list
         if (!result.hasErrors()) {
             bidListService.addBidList(bid);
             model.addAttribute("bidlists", bidListService.getBidLists());
@@ -44,18 +56,30 @@ public class BidListController {
         return "redirect:/bidList/add";
     }
 
+    /**
+     * Méthode get de la page d'update de l'objet BidList
+     * @param id
+     * @param model
+     * @return String
+     */
     @GetMapping("/bidList/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Bid by Id and to model then show to the form
         BidList bidList = bidListService.getBidListById(id).orElseThrow(() -> new IllegalArgumentException("Invalide bidList id : " + id));
         model.addAttribute("bidList", bidList);
         return "bidList/update";
     }
 
+    /**
+     * Méthode post de l'update de l'objet BidList
+     * @param id
+     * @param bidList
+     * @param result
+     * @param model
+     * @return String
+     */
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Bid and return list Bid
         if (result.hasErrors()) {
             return "redirect:/bidList/update";
         }
@@ -65,9 +89,14 @@ public class BidListController {
         return "redirect:/bidList/list";
     }
 
+    /**
+     * Méthode get de la page de suppression de l'objet BidList
+     * @param id
+     * @param model
+     * @return String
+     */
     @GetMapping("/bidList/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Bid by Id and delete the bid, return to Bid list
         BidList bidList = bidListService.getBidListById(id).orElseThrow(() -> new IllegalArgumentException("Invalide bidList id : " + id));
         bidListService.deleteBidListById(bidList.getBidListId());
         model.addAttribute("bidLists", bidListService.getBidLists());
