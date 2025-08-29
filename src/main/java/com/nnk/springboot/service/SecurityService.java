677b0dda6262cee4ed22b1052308658a4414e103
David <davidroussel171113@outlook.fr>
2025-08-29 17:20:08 +0200

    Clean code

--- src/main/java/com/nnk/springboot/service/SecurityService.java
+++ src/main/java/com/nnk/springboot/service/SecurityService.java
@@ -12,24 +12,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityService {
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    /**
-     * Méthode permettant de mettre à jour les utilisateurs authentifier
-     * @param user
-     */
-    public void updateSpringSecurityContext(User user) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
-        UsernamePasswordAuthenticationToken newAuthentication = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                authentication.getCredentials(),
-                userDetails.getAuthorities()
-        );
-        SecurityContextHolder.getContext().setAuthentication(newAuthentication);
-    }
-
     /**
      * Méthode permettant de vérifier l'authentification dans un context sécurisé
      * @return true si authentifier false si non
